7f4356154bbde724d5cab25653154b1a49342682
Zlatin Hristov <zlatin.hristov@softavis.com>
2024-10-16 23:18:46 +0300

    Initial upload

---
+++ coverage-check.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(-1);
+
+defined('STDERR') || define('STDERR', fopen('php://stderr', 'wb'));
+
+$threshold = $argv[1] ?? 60;
+$filename = $argv[2] ?? 'coverage.xml';
+
+if (PHP_SAPI !== 'cli') {
+    echo "\"bin/check-coverage\" should be invoked via the CLI version on PHP\n";
+
+    exit(1);
+}
+if (!filter_var($threshold, FILTER_VALIDATE_INT)) {
+    echo "Threshold parameter must be integer ({$threshold} given)\n";
+
+    exit(1);
+}
+
+if (!file_exists($filename)) {
+    echo "Coverage file \"{$filename}\" not found\n";
+
+    exit(1);
+}
+
+$xmlContent = file_get_contents($filename);
+try {
+    $xml = new SimpleXMLElement($xmlContent);
+} catch (Exception $e) {
+    echo "Coverage file \"{$filename} is empty or broken\n";
+
+    exit(1);
+}
+
+$elements = 0;
+$coveredElements = 0;
+
+$metrics = $xml->xpath('//metrics');
+
+foreach ($metrics as $metric) {
+    $elements += $metric['elements'];
+    $coveredElements += $metric['coveredelements'];
+}
+
+$coverage = round(($coveredElements / $elements) * 100);
+
+
+if ($coverage < $threshold) {
+    echo "Code coverage is {$coverage}%, which is below the accepted {$threshold}%\n";
+
+    exit(1);
+}
+echo "Code coverage is {$coverage}% - OK!";
+
+exit(0);
